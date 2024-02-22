630c584c4f8fb2d53cfb5030e3f6b62cc9932361
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2024-02-22 15:36:17 -0800

    started user account creation/login&signup pages

---
+++ frontend/src/Pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Input, Text } from "@chakra-ui/react"
+
+const SignUp = () => {
+    return (
+        <Box>
+            <Heading>Create an Account</Heading>
+            <Box>Form</Box>
+        </Box>
+    )
+}
+
+export default SignUp;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'; // Import BrowserRouter from react-router-dom
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
+import reportWebVitals from './reportWebVitals';
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/sign-up",
+        element: <SignUp />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-reportWebVitals();
+root.render(<RouterProvider router={router} />)

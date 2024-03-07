8e426f1c4320c65dff550c7cb2a8dacead71f944
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2024-03-07 15:39:07 -0800

    Sign Up Form Control & Error msg

--- frontend/src/Pages/SignUp.tsx
+++ frontend/src/Pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Input, Text } from "@chakra-ui/react"
+import { Box, Button, FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, Text } from "@chakra-ui/react"
 import { useState } from "react"
 import axios from 'axios';
 
@@ -8,45 +8,77 @@ const SignUp = () => {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    // const [submitClicked, setSubmitClicked] = useState(false);
+
+    const [submitClickedName, setSubmitClickedName] = useState(false);
+    const [submitClickedEmail, setSubmitClickedEmail] = useState(false);
+    const [submitClickedUsername, setSubmitClickedUsername] = useState(false);
+    const [submitClickedPassword, setSubmitClickedPassword] = useState(false);
+
+    const isErrorName = name === "" && submitClickedName;
+    const isErrorEmail = email === "" && submitClickedEmail;
+    const isErrorUsername = username === "" && submitClickedUsername;
+    const isErrorPassword = password === "" && submitClickedPassword;
 
     const onChangeName = (e: any) => {
-        console.log(e.target.value);
+        setSubmitClickedName(false);
         setName(e.target.value);
     }
 
     const onChangeEmail = (e: any) => {
-        console.log(e.target.value);
+        setSubmitClickedEmail(false);
         setEmail(e.target.value);
     }
 
     const onChangeUsername = (e: any) => {
-        console.log(e.target.value);
+        setSubmitClickedUsername(false);
         setUsername(e.target.value);
     }
 
     const onChangePassword = (e: any) => {
-        console.log(e.target.value);
+        setSubmitClickedPassword(false);
         setPassword(e.target.value);
     }
 
     const onSubmit = () => {
-        axios
-        .post("http://localhost:3005/auth/sign-up", {
-            name,
-            email,
-            username,
-            password,
-        }).then((response: any) => { // You can declare 'response' as 'any' or use the actual type if you know it
-            console.log('RESPONSE', response);
-        }).catch((error: any) => {
-            console.error('ERROR', error);
-        });
-    }
+
+        setSubmitClickedName(true);
+        setSubmitClickedEmail(true);
+        setSubmitClickedUsername(true);
+        setSubmitClickedPassword(true);
+
+        if (name === "" || email === "" || username === "" || password === "") {
+            console.log("Error!")
+        } else {
+            axios
+            .post("http://localhost:3005/auth/sign-up", {
+                name,
+                email,
+                username,
+                password,
+            }).then((response: any) => { // You can declare 'response' as 'any' or use the actual type if you know it
+                console.log('RESPONSE', response);
+                setName("");
+                setEmail("");
+                setUsername("");
+                setPassword("");
+                setSubmitClickedName(false);
+                setSubmitClickedEmail(false);
+                setSubmitClickedUsername(false);
+                setSubmitClickedPassword(false);
+            }).catch((error: any) => {
+                console.error('ERROR', error);
+            });
+        }
+        }
+
 
 
     return (
         <Box>
-            <Text textAlign="center" mb={4} fontSize={20}>Create an Account</Text>
+            <Text textAlign="center" mb={4} fontSize={20}>
+                Create an Account
+            </Text>
             <Box
             maxW="75%"
             display="flex"
@@ -55,22 +87,55 @@ const SignUp = () => {
             margin="0 auto"
             gap={4}
             >
+
+            <FormControl isInvalid={isErrorName} isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input type='text' value={name} onChange={onChangeName} />
+                {!isErrorName ? null : (
+                    <FormErrorMessage>Name is required.</FormErrorMessage>
+                )}
+            </FormControl>
+
+            <FormControl isInvalid={isErrorEmail} isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input type='email' value={email} onChange={onChangeEmail} />
+                {!isErrorEmail ? null : (
+                    <FormErrorMessage>Valid email address is required.</FormErrorMessage>
+                )}
+            </FormControl>
+
+            <FormControl isInvalid={isErrorUsername} isRequired>
+                <FormLabel>Username</FormLabel>
+                <Input type='text' value={username} onChange={onChangeUsername} />
+                {!isErrorUsername? null : (
+                    <FormErrorMessage>Username is required.</FormErrorMessage>
+                )}
+            </FormControl>
+
+            <FormControl isInvalid={isErrorPassword} isRequired>
+                <FormLabel>Password</FormLabel>
+                <Input type='password' value={password} onChange={onChangePassword} />
+                {!isErrorPassword? null : (
+                    <FormErrorMessage>Password is required.</FormErrorMessage>
+                )}
+            </FormControl>
+{/*
                 <Box display="flex" flexDirection="column" gap={2} w="100">
                     <Text>Name: </Text>
-                    <Input type="text" onChange={onChangeName} />
-                </Box>
-                <Box display="flex" flexDirection="column" gap={2} w="100">
+                    <Input type="text" onChange={onChangeName} value={name}/>
+                </Box> */}
+                {/* <Box display="flex" flexDirection="column" gap={2} w="100">
                     <Text>Email Address: </Text>
-                    <Input type="email" onChange={onChangeEmail}/>
-                </Box>
-                <Box display="flex" flexDirection="column" gap={2} w="100">
+                    <Input type="email" onChange={onChangeEmail} value={email}/>
+                </Box> */}
+                {/* <Box display="flex" flexDirection="column" gap={2} w="100">
                     <Text>Username: </Text>
-                    <Input type="text" onChange={onChangeUsername}/>
+                    <Input type="text" onChange={onChangeUsername} value={username}/>
                 </Box>
                 <Box display="flex" flexDirection="column" gap={2} w="100">
                     <Text>Password: </Text>
-                    <Input type="password" onChange={onChangePassword}/>
-                </Box>
+                    <Input type="password" onChange={onChangePassword} value={password}/>
+                </Box> */}
                 <Button w="100" onClick={onSubmit}>Submit</Button>
             </Box>
         </Box>
